35670a438cc0ebf8ea2daf8c7b364ed9cafa0e31
Pratik Prajapati <prajapatipratik20@gmail.com>
2024-02-28 14:48:45 -0500

    Demo CI/CD design

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload Modified.
+          Edit <code>src/App.js</code> and First Devops app.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React-1
+          Learn React
         </a>
       </header>
     </div>
