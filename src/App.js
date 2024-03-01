7e6d725489aab267fc32b537e60436b02087f422
Pratik Prajapati <prajapatipratik20@gmail.com>
2024-03-01 16:01:31 -0500

    CI/CD Testing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and First Devops app Modified.
+          Edit <code>src/App.js</code> and First Devops app -1.
         </p>
         <a
           className="App-link"
